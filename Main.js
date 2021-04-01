28c584fd1a8ffdad1f516bc5c7397f5a14c4a228
Tegsingh1 <78391323+Tegsingh1@users.noreply.github.com>
2021-04-01 12:19:41 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,17 @@
+<div class="page-header" id="School Achievements">
+                <h2 class="text-capitalize"> School Achievements </h2>
+            </div>
+            <div class="bg-primary movie-head" data-toggle="collapse" data-target="#School_Achievements"> Elementary FI program overview and transportation <i class="fa-fa-sort-desc"> </i>
+            </div>
+
+            <div id="#School_Achievements" class="collapse">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 " id="achievementsID">
+                    <img src="https://www.fraserinstitute.org/sites/default/files/styles/large/public/ontario-elementary-school-rankings-2017.jpg?itok=9BS3xTxR" class="img-responsive img-thumbnail">
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 movie_text">
+
+                    <h5 class="text-success text-left text-capitalize">From grades 1 to 8, FI students spend half of their school day learning in French and the other half learning in English. </h5>
+                    <p> The Peel board provides busing for students in the elementary FI program. Depending on how far a family lives from the nearest FI school, an elementary child may qualify for busing. The board determines busing based on the age of the students and the distance from the school.</p>
+
+                </div>
+            </div>
